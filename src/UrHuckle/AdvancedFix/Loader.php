318b44e69798e4967349de8fb00826c99b7e61de
diamondkorben <46268645+diamondkorben@users.noreply.github.com>
2021-02-05 22:57:27 -0500

    Update Loader.php

--- src/UrHuckle/AdvancedFix/Loader.php
+++ src/UrHuckle/AdvancedFix/Loader.php
@@ -1,1 +1,14 @@
 <?php
+
+use UrHuckle\AdvancedFix\commands\FixCommand;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase {
+    public function onEnable(){
+        if(!is_dir($this->getDataFolder())){
+            mkdir($this->getDataFolder());
+        }
+        $this->saveDefaultConfig();
+        $this->getLogger()->info("Enabled");
+    }
+}
